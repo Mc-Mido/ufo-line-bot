427ffc8de4df0d71ff05a5031fa52c41065c75c0
Benjamin Dupont <benjamin-dupont@harvest.fr>
2021-11-20 23:54:49 +0100

    Fix import

--- ufo-bot-spring-boot/src/main/java/fr/ufo/bot/spring/tagall/application/TagallApplication.java
+++ ufo-bot-spring-boot/src/main/java/fr/ufo/bot/spring/tagall/application/TagallApplication.java
@@ -1,12 +1,13 @@
 package fr.ufo.bot.spring.tagall.application;
 
+import fr.ufo.bot.spring.tagall.config.TagallConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(TagallApplication.class)
+@Import(TagallConfiguration.class)
 public class TagallApplication
 {
     public static void main(final String[] args)
